f7156807961853507c28ad4b79c3dfd131ad0826
Edelis1931 <xinchi.chen@mail.mcgill.ca>
2017-12-27 09:32:50 +0800

    69c

---
+++ cpp/69sqrtx.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long start = 0;
+        long end = x;
+        long mid = 0;
+        
+        if (x == 0 || x == 1)
+        {
+            return x;
+        }
+        
+        while (start + 1 < end)
+        {
+            mid = start + (end - start) / 2;
+            
+            if (mid*mid <= x)
+            {
+                start = mid;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        
+        if (end*end == x)
+        {
+            return end;
+        }
+        else
+        {
+            return start;
+        }
+    }
+};
