244f8c54257f29f4d173f024afc6e65cd8ad6b07
Edelis1931 <xinchi.chen@mail.mcgill.ca>
2018-01-03 17:29:49 +0800

    format

--- java/153_findMinimumInRotatedSortedArray.java
+++ java/153_findMinimumInRotatedSortedArray.java
@@ -2,20 +2,20 @@ class Solution {
     public int findMin(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
-        
+
         if (end == -1)
         {
             return 0;
         }
 
         int target = nums[end];
-        
+
         int mid = 0;
-        
+
         while (start + 1 < end)
         {
             mid = start + (end - start) / 2;
-            
+
             if (nums[mid] < target)
             {
                 end = mid;
@@ -25,10 +25,10 @@ class Solution {
                 start = mid;
             }
         }
-        
+
         System.out.println("start:" + nums[start]);
         System.out.println("end:" + nums[end]);
-        
+
         if (nums[start] <= target)
         {
             return nums[start];
