54391b5ff8c899d54387203b82b0b7f5cef0007f
Edelis1931 <xinchi.chen@mail.mcgill.ca>
2018-04-21 00:01:57 -0400

    update

--- 418.java
+++ 418.java
@@ -1,6 +1,14 @@
-20180418,r,135,candy
-20180418,r,136,single number
-20180419,r,139,word break
-20180419,r,141,linked list cycle
-20180419,r,142,linked list cycle ii
-20180419,r,150,evaluate reverse polish notation
+!:solution not optimized, cannot pass follow up
+
+20180418,r,135,Candy
+20180418,r,136,Single Number
+20180419,r,139,Word Break
+20180419,r,141,Linked List Cycle
+20180419,r,142,Cinked List Cycle ii
+20180419,r,150,Evaluate Reverse Polish Notation
+20180420,n,117,Populating Next Right Pointers in Each Node II
+20180420,n,108,Convert Sorted Array to Binary Search Tree
+20180420,n,230,Kth Smallest Element in a BST !
+20180420,n,463,Island Perimeter
+20180420,n,200,Number of Islands
+20180420,n,286,Walls and Gates
